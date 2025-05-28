e1fc436f962cb21f8daf6e3ec12b5a7d0d4d1214
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-05-28 13:52:29 +0200

    feat: implement swagger file.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+import * as yamljs from 'yamljs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const document: OpenAPIObject = yamljs.load('doc/api.yaml');
+  SwaggerModule.setup('api', app, document);
+  
   process.on('uncaughtException', (error) => {
     console.error('Uncaught Exception:', error);
     process.exit(1);
@@ -13,6 +18,13 @@ async function bootstrap() {
     console.error('Unhandled Rejection:', reason);
     process.exit(1);
   });
+
+  // Add OpenApi documentation
+  const options = {
+    openApi: {
+      enabled: true,  // Enable OpenAPI documentation
+    },
+  };
 
   await app.listen(process.env.PORT ?? 4000);
   console.log(`Application is running on: ${await app.getUrl()}`);
